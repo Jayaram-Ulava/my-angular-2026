5a4188f8fe8b156996b63bc658d530c3799bafa9
Jayaram-Ulava <jayaram.ulava@gmail.com>
2026-03-03 11:59:41 +0530

    strucatiral directives

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { WellcomeComponent } from './wellcome/wellcome.component';
 import { HomeComponent } from './home/home.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 
 const routes: Routes = [
@@ -14,7 +15,8 @@ const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'wellcome',component:WellcomeComponent},
     {path:'databinding',component:DataBindingComponent},
-    {path:'calculator',component:CalculatorComponent}
+    {path:'calculator',component:CalculatorComponent},
+    {path:'directives',component:DirectivesComponent}
   ]},
   {path:'login',component:LoginComponent},
   {path:'',component:LoginComponent},
---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css']
+})
+export class DirectivesComponent {
+
+  cities:string[] = ['Hyderabad', 'Bangalore', 'Chennai', 'Mumbai'];
+
+
+  products :any= [
+    {name:'pen',price:20,color:'red'},
+    {name:'pencil',price:10,color:'blue'},
+    {name:'book',price:100,color:'green'},
+    {name:'bag',price:500,color:'yellow'},
+    {name:'laptop',price:50000,color:'black'},
+    {name:'mobile',price:20000,color:'white'},
+    {name:'watch',price:2000,color:'silver'}
+  ]
+
+  showHeader:boolean = false;
+}
